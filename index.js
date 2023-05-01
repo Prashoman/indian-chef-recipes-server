e11eac15c6bfde69e0eca2d44a8c4bd21760d047
Badhon <prashomanchakrabarti1@gmail.com>
2023-05-02 01:01:24 +0600

    created slider api and get method call slider api and install cros

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const slider = require("./data/slider.json");
+const cors = require("cors");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Indian Recipes server is running!");
+});
+app.get("/slider", (req, res) => {
+  res.send(slider);
 });
 
 app.listen(port, () => {
