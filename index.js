f42d5b9785b15c7226234b322fa3c9ff8a5fbe6b
Badhon <prashomanchakrabarti1@gmail.com>
2023-05-02 11:40:42 +0600

    create the recipesdetails api and route setup

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const slider = require("./data/slider.json");
+const chef = require("./data/chef.json");
+const recipesDetails = require("./data/recipesdetails.json");
 const cors = require("cors");
 
 app.use(cors());
@@ -11,6 +13,24 @@ app.get("/", (req, res) => {
 });
 app.get("/slider", (req, res) => {
   res.send(slider);
+});
+
+app.get("/chef", (req, res) => {
+  res.send(chef);
+});
+app.get("/chef/:id", (req, res) => {
+  const id = req.params.id;
+  const singleChef = chef.find((chef) => chef.id == id);
+  if (singleChef) {
+    res.send(singleChef);
+  }
+});
+app.get("/recipes/details/:id", (req, res) => {
+  const id = req.params.id;
+  const recipesFilter = recipesDetails.filter((recipe) => recipe.chefId == id);
+  if (recipesDetails) {
+    res.send(recipesFilter);
+  }
 });
 
 app.listen(port, () => {
